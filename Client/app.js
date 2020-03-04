4bbf79727aa41d49e25be22893e8ea9850a9549a
Austin Barry <Ajpbarry@gmail.com>
2020-03-04 13:39:28 -0600

    added some logic to the edit method, currently not working

--- Client/app.js
+++ Client/app.js
@@ -23,15 +23,33 @@ function ajax(url, type, info, htmlMethod) {
         };
         ajax('https://localhost:44325/api/movie', 'post', JSON.stringify(movie),
         function(data){
-            console.log(data);
-            $('#response pre').html(data);
-            });
+            
+        })
         e.preventDefault();
     }
     $('#my-form').submit(addMovieToDataBase);
 })
 (jQuery);
 
+function editMovie(movieId){
+    var title = promptFor("What should the title be?", chars);
+    var genre = promptFor("Who is the director?", chars);
+    var directorName = promptFor("What should the genre be?", chars);
+
+    var movie = {
+        Title: title,
+        Genre: genre,
+        DirectorName: directorName
+    };
+
+    ajax('https://localhost:44325/api/movie/' + movieId, 'put', movie,
+    function(data){
+        console.log("sucess?")
+
+    })
+
+
+}
 
 function makeTable() {
     ajax('https://localhost:44325/api/movie', 'get', null, function (data) {    
@@ -40,16 +58,32 @@ function makeTable() {
 };
         
 
-function tableAppend(i ,value){
-
-    console.log(value);
-    var rows = "<tr>" + 
-                "<td id='title'>" + value.title + "</td>" + 
-                "<td id='directorName'>" + value.directorName + "</td>" + 
-                "<td id='genre'>" + value.genre + "</td>" +
-                "<td id='edit'><button type='submit'>Submit</button></td>" +
+function tableAppend(i ,movie){
+var rows = "<tr>" + 
+                "<td id='title'>" + movie.title + "</td>" + 
+                "<td id='directorName'>" + movie.directorName + "</td>" + 
+                "<td id='genre'>" + movie.genre + "</td>" +
+                "<td id='edit'><button type='button' onclick = editMovie(" + movie.movieId + ")>Edit</button></td>" +
                 "</tr>";
                  table.append(rows);
 }
 
 makeTable();
+
+// function that prompts and validates user input
+function promptFor(question, callback){
+    do{
+      var response = prompt(question).trim();
+    } while(!response || !callback(response));
+    return response;
+  }
+  
+  // helper function to pass into promptFor to validate yes/no answers
+  function yesNo(input){
+    return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
+  }
+  
+  // helper function to pass in as default promptFor validation
+  function chars(input){
+    return true; // default validation only
+  }
